d77d7577b47f2200e422d075c42969bf0379654a
Katja Makarova <47348068+dudlish@users.noreply.github.com>
2020-08-14 11:53:41 +0300

    Updates

---
+++ precache-manifest.26f22306c70c76e78b03366b3f38a284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a92e2c7f80da7ff24095ab367309e17",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "993e1bb0f8d9e1a8f781",
+    "url": "/monsters-rolodex/static/css/main.10c4f38c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993e1bb0f8d9e1a8f781",
+    "url": "/monsters-rolodex/static/js/main.df05daab.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
